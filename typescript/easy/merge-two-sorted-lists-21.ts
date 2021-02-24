96bb7300d07ac0c77bfe2503e00c81b0cdd38ca9
GerBawn <lingchen410@163.com>
2021-02-25 00:05:50 +0800

    Create merge-two-sorted-lists-21.ts

---
+++ typescript/easy/merge-two-sorted-lists-21.ts
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+function mergeTwoLists(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  let root: ListNode | null = new ListNode(-1);
+  let cur = root;
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      cur.next = l1;
+      l1 = l1.next;
+    } else {
+      cur.next = l2;
+      l2 = l2.next;
+    }
+    cur = cur.next;
+  }
+  cur.next = l1 || l2;
+
+  return root.next;
+}
+
+const l1 = new ListNode(1);
+l1.next = new ListNode(2);
+l1.next.next = new ListNode(4);
+
+const l2 = new ListNode(1);
+l2.next = new ListNode(3);
+l2.next.next = new ListNode(4);
+let root = mergeTwoLists(l1, l2);
+while (root !== null) {
+  console.log(root.val);
+  root = root.next;
+}
+export {};
